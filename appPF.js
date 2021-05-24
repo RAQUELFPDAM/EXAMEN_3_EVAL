796476d511fa6542376551bfc708e07a8fcf3be4
Raquel Simó Jiménez <raquelfpdam@gmail.com>
2021-05-24 11:22:17 +0200

    Subiendo segunda parte del examen final

--- appPF.js
+++ appPF.js
@@ -6,8 +6,12 @@ var entornos;
 var entF;
 var lenguaje;
 var lengF;
+var ingles;
+var inglesF;
 var media;
+var mediaCon
 var resultado;
+var resultado2;
 
 
 function calcularMedia(){
@@ -24,4 +28,27 @@ function calcularMedia(){
     resultado.value=media.toFixed(2);
     console.log(media.toFixed(2));
     return false;
+}
+
+function calcularMediaCon(){
+    programacion= document.getElementById("prog").value;
+    progF=parseFloat(programacion);
+    basededatos= document.getElementById("bd").value;
+    bdF=parseFloat(basededatos);
+    entornos=document.getElementById("ent").value;
+    entF=parseFloat(entornos);
+    lenguaje=document.getElementById("leng").value;
+    lengF=parseFloat(lenguaje);
+    ingles= document.getElementById("ing").value;
+    inglesF = parseFloat(ingles);
+    mediaCon=(progF+bdF+entF+lengF+inglesF)/5;
+    resultado2=document.getElementById("mediaCon");
+    resultado2.value=mediaCon.toFixed(2);
+    console.log(mediaCon.toFixed(2));
+    return false;
+}
+
+function examen(){
+    calcularMediaCon();
+    calcularMedia();
 }
